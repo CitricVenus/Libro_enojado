f72ff6b173fcb0b3aba660d1724c652f23bee0f8
Erick Alfonso Montan Lopez <inquisidor.890@hotmail.com>
2018-02-08 17:15:45 -0600

    Add files via upload

---
+++ libr.java
@@ -0,0 +1,42 @@
+public class libr{
+  private String Autor,
+                Editorial,
+                Titulo,
+                ISBN;
+  private  int cantidad;
+  public libr(string Autor, string Editorial, string Titulo, string ISBN,int cantidad){
+      this.Autor="";
+      this.Editorial="";
+      this.Titulo="";
+      this.ISBN="";
+      this.int=0;
+  }
+  public void setautor(){
+    this.Autor=autor;
+  }
+  public  void setedit(){
+    this.Editorial=edit;
+  }
+  public void settit(){
+    this.Titulo=tit;
+  }
+  public void setisb(){
+    this.ISBN=isbn;
+  }
+  public void setcant(){
+    this.cantidad=cant;
+  }
+  public String getautor(){
+    return this.autor;
+  }
+  public String getedit(){
+    return this.Editorial;
+  }
+  public String gettit(){
+    return this.Titulo;
+  }
+  public int getcant(){
+    return this.cantidad;
+  }
+
+}
